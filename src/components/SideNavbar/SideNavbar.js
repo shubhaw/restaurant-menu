c51a61865d10b0f7d40c946a1b9aa7c795ae9db4
Shubhaw Kumar <dev.shubhaw@gmail.com>
2020-09-25 00:50:32 +0530

    added sliding animation in the sidebar

--- src/components/SideNavbar/SideNavbar.js
+++ src/components/SideNavbar/SideNavbar.js
@@ -5,9 +5,9 @@ const SideNavbar = props => {
     return <div className={props.open ? classes.SideNavbarContainer : classes.Hide}>
         <div className={classes.Overlay}>
             <div className={classes.NavmenuItemHeading} onClick={() => props.onMainMenuChange("foodMenu")}>Food Menu</div>
-            {props.selectedMenu === "foodMenu" && props.foodMenu.map(item => <div className={classes.NavmenuItem}>{item.title}</div>)}
+            {props.selectedMenu === "foodMenu" && props.foodMenu.map(item => <div key={item.title} className={classes.NavmenuItem}>{item.title}</div>)}
             <div className={classes.NavmenuItemHeading} onClick={() => props.onMainMenuChange("barMenu")}>Bar Menu</div>
-            {props.selectedMenu === "barMenu" && props.barMenu.map(item => <div className={classes.NavmenuItem}>{item.title}</div>)}
+            {props.selectedMenu === "barMenu" && props.barMenu.map(item => <div key={item.title} className={classes.NavmenuItem}>{item.title}</div>)}
         </div>
     </div>
 }
