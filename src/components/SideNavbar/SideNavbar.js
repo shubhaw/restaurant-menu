4c8e40c9466e6e3e866e10f1e6349c64297ec4b1
Shubhaw Kumar <shukumar@microsoft.com>
2021-01-05 14:52:07 +0530

    Reverted the last change. Now food menu is enabled again.

--- src/components/SideNavbar/SideNavbar.js
+++ src/components/SideNavbar/SideNavbar.js
@@ -9,8 +9,8 @@ const SideNavbar = props => {
 
     return <div className={props.open ? classes.SideNavbarContainer : classes.Hide}>
         <div className={classes.Overlay}>
-            {/* <div className={classes.NavmenuItemHeading} onClick={() => props.onMainMenuChange("foodMenu")}>Food Menu</div>
-            {props.selectedMenu === "foodMenu" && props.foodMenu.map((item, index) => <div key={item.title} className={classes.NavmenuItem} onClick={() => onSubMenuChange(index)}>{item.title}</div>)} */}
+            <div className={classes.NavmenuItemHeading} onClick={() => props.onMainMenuChange("foodMenu")}>Food Menu</div>
+            {props.selectedMenu === "foodMenu" && props.foodMenu.map((item, index) => <div key={item.title} className={classes.NavmenuItem} onClick={() => onSubMenuChange(index)}>{item.title}</div>)}
             <div className={classes.NavmenuItemHeading} onClick={() => props.onMainMenuChange("barMenu")}>Bar Menu</div>
             {props.selectedMenu === "barMenu" && props.barMenu.map((item, index) => <div key={item.title} className={classes.NavmenuItem} onClick={() => onSubMenuChange(index)}>{item.title}</div>)}
         </div>
